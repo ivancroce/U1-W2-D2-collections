51149c74595a10c2bab03949567e30bee2863f60
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-06-24 16:21:05 +0100

    exs2 to continue

--- src/exs2/Main.java
+++ src/exs2/Main.java
@@ -1,8 +1,33 @@
 package exs2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 // List - ArrayList
 public class Main {
-    public static void main(String[] args) {
+    // ordered list of random n from 0 to 100
+    public static List<Integer> createOrderedList(int n) {
+        List<Integer> randomList = new ArrayList<>();
+        Random random = new Random();
 
+        for (int i = 0; i < n; i++) {
+            randomList.add(random.nextInt(101));
+        }
+
+        randomList.sort(null);
+
+        return randomList;
+    }
+
+    // clone list / reverse order
+    public static List<Integer> createCloneList(List<Integer> originalList) {
+
+        List<Integer> cloneList = new ArrayList<>(originalList);
+// to continue
+
+    }
+
+    public static void main(String[] args) {
     }
 }
